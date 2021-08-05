cbb7ed3b8ae0831df246cc98147e1a5dbb742df2
MaitrayeeK <87898010+MaitrayeeK@users.noreply.github.com>
2021-08-05 23:23:16 +0530

    Add files via upload
    Assignment 3 Submitted

---
+++ Assignment3/js.js
@@ -0,0 +1,10 @@
+
+
+function OnClick()
+{
+    let name=document.querySelector("#inp1").value;
+    document.querySelector(".div").innerText=`${name}`;
+    let city=document.querySelector("#inp2").value;
+    document.querySelector(".div1").innerText=`${city}`;
+    
+}
