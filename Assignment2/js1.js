d513fdb6cdd5e0ef5e22bd62b08adeb4a14e3b54
MaitrayeeK <87898010+MaitrayeeK@users.noreply.github.com>
2021-08-05 22:39:27 +0530

    Add files via upload
    Assignment 2 submitted

---
+++ Assignment2/js1.js
@@ -0,0 +1,6 @@
+let names=["name1","name2","name3","name4","name5"];
+
+for(i=4;i>=0;i--)
+{
+    console.log(names[i]);
+}
---
+++ Assignment2/js2.js
@@ -0,0 +1,8 @@
+console.log("Numbers divisible by 5(between 1 to 50)")
+
+for(i=1;i<=50;i++)
+{
+    if(i%5==0){
+        console.log(i);
+    }
+}
